0c5c14fa0a2e2becec004c3a21a391cbbeac768d
attobra <nora.attobra@gmail.com>
2020-10-16 18:09:49 -0400

    add bmi calculator

--- calculator.js
+++ calculator.js
@@ -13,9 +13,24 @@ app.post("/", (req, res)=>{
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
 
-    var result = num1 + num2;
+    var result = num1 + num2
 
-    res.send("The result of the calculation is " + result)
+    res.send("The result of your calculation is " + result)
+})
+
+
+app.get("/bmiCalculator", (req, res)=>{
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmiCalculator", (req, res)=>{
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight/(height**2);
+
+    res.send("Your BMI is " + bmi)
 })
 
 app.listen(3000, ()=>{
